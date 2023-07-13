0d38c240a33b90175907de05d7c0342225707b2a
Always Member007 <117436302+alwaysmember007@users.noreply.github.com>
2023-07-13 15:47:08 +0530

    Update Name.java

--- Name.java
+++ Name.java
@@ -1,1 +1,17 @@
+package com.mariten.kanatools;
+import com.mariten.kanatools.KanaAppraiser;
 
+public abstract class KanaAppraiserTester
+{
+    public KanaAppraiserTester()
+    {
+        // Test instantiation
+        KanaAppraiser kana_evaluator_object = new KanaAppraiser();
+    }
+
+    public KanaAppraiserTesterCopy()
+    {
+        // Test instantiation copy
+        KanaAppraiser kana_evaluator_object = new KanaAppraiser();
+    }
+}
